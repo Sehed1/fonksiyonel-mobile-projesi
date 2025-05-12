24db618588a2f2acf4cabafb14e84219e5d8e8a1
Sehed1 <shehedhatib@gmail.com>
2025-05-12 03:08:52 +0300

    complete backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,9 @@
-import mongoose, { connect } from "mongoose";
+import mongoose from "mongoose";
 
-export const connectDB = async() => {
-    await mongoose.connect('mongodb+srv://greatstack:1234123@cluster0.3xpkeaq.mongodb.net/TastyFoodMobileApp').then(()=> console.log("DB connected"))
+export const  connectDB = async () =>{
+
+    await mongoose.connect('mongodb+srv://greatstack:123123123@cluster0.3xpkeaq.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+   
 }
+ 
+ 
